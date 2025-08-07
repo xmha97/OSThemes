a549504632812fdc7f984b7eb2367380ef15c727
Muhammadhussein Ammari <xmha97@gmail.com>
2025-08-08 02:44:09 +0330

    Add GNOME support

--- make.cmd
+++ make.cmd
@@ -2,3 +2,5 @@ cd tulips-light
 makecab /f package.ddf
 cd ..\tulips-dark
 makecab /f package.ddf
+powershell Compress-Archive -Path "%~dp0tulips-gnome\home" -DestinationPath "%~dp0TulipsGU.zip"
+powershell Compress-Archive -Path "%~dp0tulips-gnome\usr" -DestinationPath "%~dp0TulipsGS.zip"
